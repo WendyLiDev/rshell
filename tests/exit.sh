ff6f40d027f5da0334b589217deef85db5d7e3ac
Wendy Li <wli025@ucr.edu>
2016-11-18 10:22:00 -0800

    added test files

--- tests/exit.sh
+++ tests/exit.sh
@@ -1,1 +1,26 @@
-exit
+echo Check Parsnip.cpp exists, is file, or directory and stop program: ;
+test -e Parsnip.cpp && exit;
+[-e Parsnip.cpp] && exit;
+test -f Parsnip.cpp && exit;
+[-f Parsnip.cpp] && exit;
+test -d Parsnip.cpp && exit;
+[-d Parsnip.cpp] && exit;
+
+echo ;
+echo Check texts exists, is file, or directory and stop program: ;
+test -e texts && exit;
+[-e texts] && exit;
+test -f texts && exit;
+[-f texts] && exit;
+test -d texts && exit;
+[-d texts] && exit;
+
+echo ;
+echo Check Parser.cpp exists and is file and stop program: ;
+(test -e Parser.cpp && test -f Parser.cpp) && exit;
+
+echo ;
+echo Check tests exists and is directory and stop program: ;
+(test -e tests && test -d tests) && exit; 
+
+# file exits before last three lines, change order to test both
--- tests/test_test.sh
+++ tests/test_test.sh
@@ -1,7 +1,32 @@
-([-e Parser.cpp] && echo Parser.cpp exists) && (test -f Parser.cpp && echo Parser.cpp is file) && (test -d tests && echo tests directory found); 
+echo Parser.cpp: ;
+echo exists: && [ -e Parser.cpp ]; ( test -e Parser.cpp );
+echo file: && [ -f Parser.cpp ]; ( test -f Parser.cpp );
+echo directory: && [ -d Parser.cpp ]; ( test -d Parser.cpp );
 
-([-e Parse.cpp] && echo Parse.cpp exists) || (echo Parse.cpp does not exist);
-(test -f Parse.cpp && echo Parse.cpp is file) || (echo Parse.cpp does not exist);
+echo tests: ;
+echo exists: && [ -e tests ]; ( test -e tests );
+echo file: && [ -f tests ]; ( test -f tests );
+echo directory: && [ -d tests ]; ( test -d tests );
+
+echo ;
+echo Parsnip.cpp: ;
+echo exists: && [ -e Parsnip.cpp ]; ( test -e Parsnip.cpp );
+echo file: && [ -f Parsnip.cpp ]; ( test -f Parsnip.cpp );
+echo directory: && [ -d Parsnip.cpp ]; ( test -d Parsnip.cpp );
+
+echo ;
+echo texts: ;
+echo exists: && [ -e texts ]; ( test -e texts );
+echo file: && [ -f texts ]; (test -f texts );
+echo directory: && [ -d texts ]; ( test -d texts );
+
+echo ;
+echo Check with operators: ;
+( echo Parser.cpp exists && [-e Parser.cpp] ) && ( echo Parser.cpp is file && test -f Parser.cpp ) && ( echo tests directory found && test -d tests ); 
+
+echo ;
+([-e Parsnip.cpp] && echo Parsnip.cpp exists) || (echo Parsnip.cpp does not exist);
+(test -f Parsnip.cpp && echo Parsnip.cpp is file) || (echo Parsnip.cpp does not exist);
 (test -d texts && echo texts directory found) || (echo texts directory not found);
 
 exit;
