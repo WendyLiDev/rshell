ddb2c96126afbf6745e0e8603c5979caea82dd6f
Wendy Li <wli025@ucr.edu>
2016-12-01 21:50:28 -0800

    updated test_cd.sh file and fixed test bug

--- tests/test_cd.sh
+++ tests/test_cd.sh
@@ -1,8 +1,26 @@
+echo CONTENTS OF CURRENT DIRECTORY: ;
 ls;
+
+echo CD TO TESTS: ;
 cd tests;
-ls;
+echo;
+
+echo RETURN TO PREV WORKING DIRECTORY: ;
 cd -;
-ls;
+cd -;
+cd -;
+echo;
+
+(test -d test && test -e test) && (cd test && echo moved to test directory);
+
+(test -d tests && test -e tests) && (cd tests && echo moved to tests directories);
+
+cd -;
+
+(test -d tests && test -e tests) && (cd tests && [-e exit.sh]);
+
+echo RETURN TO HOME DIRECTORY: ;
 cd;
-ls;
+echo;
+
 exit;
